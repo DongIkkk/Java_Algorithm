e6795971f6a5bc02fca12bbb1745e2218c583c54
DongIkkk <w8h0412@gmail.com>
2023-01-24 22:36:12 +0900

    algorithm study swea 1204

---
+++ src/swea/Sol1204.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Sol1204 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int tc = sc.nextInt();
+            System.out.printf("#%d ", tc);
+
+            int[] arr = new int[101];
+            for (int i=0; i<1000; i++){
+                int score = sc.nextInt();
+                arr[score]++;
+            }
+            int max = 0;
+            int max_index = 0;
+            for(int i=0; i<101; i++){
+                if(arr[i]>=max){
+                    max = arr[i];
+                    max_index = i;
+                }
+            }
+            System.out.printf("%d\n", max_index);
+        }
+    }
+}
