a17ec833eeab3e42e5cc186438a4b11813845452
DongIkkk <w8h0412@gmail.com>
2023-02-19 22:04:28 +0900

    swea d3 1289

---
+++ src/swea/D3/Sol1289.java
@@ -0,0 +1,24 @@
+package D3;
+
+import java.util.Scanner;
+
+public class Sol1289 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)      {
+            String input = sc.next();
+            String[] arr = input.split("");
+            String check = "0";
+            int count = 0;
+            for (String s: arr) {
+                if(check.hashCode() != s.hashCode()) {
+                    count++;
+                    check = s;
+                }
+
+            }
+            System.out.println("#"+test_case+" "+count);
+        }
+    }
+}
