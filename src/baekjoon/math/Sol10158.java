68e54a1e6c73390fd9fe380385690e3d525ab72a
DongIkkk <w8h0412@gmail.com>
2023-03-10 00:30:54 +0900

    개미 구글

--- src/baekjoon/math/Sol10158.java
+++ src/baekjoon/math/Sol10158.java
@@ -1,18 +1,30 @@
 package math;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Sol10158 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int w = sc.nextInt();
-        int h = sc.nextInt();
-        int p = sc.nextInt();
-        int q = sc.nextInt();
-        int t = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in = br.readLine();
+        int w = Integer.parseInt(in.split(" ")[0]);
+        int h = Integer.parseInt(in.split(" ")[1]);
+        in = br.readLine();
+        int p = Integer.parseInt(in.split(" ")[0]);
+        int q = Integer.parseInt(in.split(" ")[1]);
+        int t = Integer.parseInt(br.readLine());
 
+        if(((int)(p + t) / w) % 2 == 0) {
+            p = (p + t) % w;
+        }else {
+            p = w - (p + t) % w;
+        }
 
-
-        System.out.println(p+" "+q);
+        if(((int)(q + t) / h) % 2 == 0) {
+            q = (q + t) % h;
+        }else {
+            q = h - (q + t) % h;
+        }
+        System.out.println(p + " " + q);
     }
 }
