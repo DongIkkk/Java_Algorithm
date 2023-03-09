e9025d3a684d9bc1d54821dd9fdf7497912dd3a4
DongIkkk <w8h0412@gmail.com>
2023-03-09 15:21:42 +0900

    0309 BOJ 11729 hanoi

--- src/baekjoon/recursive/Sol11729.java
+++ src/baekjoon/recursive/Sol11729.java
@@ -3,18 +3,33 @@ package recursive;
 import java.util.Scanner;
 
 public class Sol11729 {
+    static StringBuilder sb = new StringBuilder();
+    static int count=0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int x = 0;
-        if(N%2==1) x=3;
-        else x=2;
-        hanoi(N,1, x);
+        hanoi(N,1, 2, 3);
+        System.out.println(count);
+        System.out.print(sb.toString());
 
     }
-    public static void hanoi(int N, int start, int end){
+    public static void hanoi(int N, int start, int mid, int end){
+            if(N==1) {
+                sb.append(start);
+                sb.append(" ");
+                sb.append(end);
+                sb.append("\n");
+                count++;
+                return;
+            }
 
-
+            hanoi(N-1, start, end, mid);
+            sb.append(start);
+            sb.append(" ");
+            sb.append(end);
+            sb.append("\n");
+            count++;
+            hanoi(N-1, mid, start, end);
 
     }
 }
