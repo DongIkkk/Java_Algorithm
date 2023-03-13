cbed35b0235919be5f89e2d21661d1704266075c
DongIkkk <w8h0412@gmail.com>
2023-03-13 19:14:02 +0900

    BOJ 1759 python

---
+++ src/baekjoon/bruteforce/Sol1759.java
@@ -0,0 +1,28 @@
+package bruteforce;
+
+import java.util.Arrays;
+import java.util.Scanner;
+// 파이썬 구현 정답처리
+public class Sol1759 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        int C = sc.nextInt();
+        char[] carr = new char[C];
+
+        for (int i = 0; i < C; i++) {
+            carr[i] = sc.next().charAt(0);
+        }
+
+        Arrays.sort(carr);
+        System.out.println(Arrays.toString(carr));
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<L; i++){
+        // 파이썬으로했음
+
+
+        }
+
+    }
+}
