c5fe97c70b0cfb511e714149420a9afc47f87eff
DongIkkk <w8h0412@gmail.com>
2023-03-23 17:33:16 +0900

    나무자르기

--- src/baekjoon/BinarySearch/Sol2805.java
+++ src/baekjoon/BinarySearch/Sol2805.java
@@ -13,38 +13,33 @@ public class Sol2805 {
         int m = Integer.parseInt(st.nextToken());
         int[] trees = new int[n];
 
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
+        long max = 0;
+        long min = 0;
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             trees[i] = Integer.parseInt(st.nextToken());
             if(max<trees[i]) max = trees[i];
-            if(min>trees[i]) min = trees[i];
         }
 
-        int h = (max+min) / 2;
+        long h =0;
 
-        while(true){
-
+        while(min<=max){
+            h = (max+min) / 2;
             long sum = 0;
             for (int i = 0; i < n; i++) {
                 if(h<trees[i]){
                     sum += trees[i] - h;
                 }
             }
-            if (sum == m) {
-                break;
-            } else if(sum > m){
-                min = h;
-                h = (max+min) / 2 + 1;
+
+            if(sum >= m){
+                min = h+1;
             } else {
-                max = h;
-                h = (max+min) / 2;
+                max = h-1;
             }
 
-            if(min>=max) break;
         }
 
-        System.out.println(h);
+        System.out.println(max);
     }
 }
