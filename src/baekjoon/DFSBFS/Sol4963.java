5b5d038a219228cedbc1251aee689c55c41aff1c
DongIkkk <w8h0412@gmail.com>
2023-03-28 16:26:42 +0900

    0328 BOJ 4963 섬의개수 재풀이 - dfs

--- src/baekjoon/DFSBFS/Sol4963.java
+++ src/baekjoon/DFSBFS/Sol4963.java
@@ -73,3 +73,62 @@ public class Sol4963 {
 
     }
 }
+// ---- 재풀이
+//public class Sol4963 {
+//    static int x, y;
+//    static int[][] map;
+//    static boolean[][] visited;
+//    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+//    static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        while(true){
+//
+//            y = sc.nextInt();
+//            x = sc.nextInt();
+//
+//            if(x==0 && y==0) break;
+//
+//            map = new int[x][y];
+//            visited = new boolean[x][y];
+//
+//            for (int i = 0; i < x; i++) {
+//                for (int j = 0; j < y; j++) {
+//                    map[i][j] = sc.nextInt();
+//                }
+//            }
+//
+//            int count = 0;
+//            for (int i = 0; i < x; i++) {
+//                for (int j = 0; j < y; j++) {
+//                    if(!visited[i][j] && map[i][j]==1){
+//                        count++;
+//                        dfs(i,j);
+//
+//                    }
+//                }
+//            }
+//
+//            System.out.println(count);
+//        }
+//
+//    }
+//
+//    static public void dfs(int r, int c){
+//        visited[r][c] = true;
+//
+//        for (int i = 0; i < 8; i++) {
+//            int nextx = r + dx[i];
+//            int nexty = c + dy[i];
+//            if(nextx>=0 && nexty>=0 && nextx<x && nexty<y){
+//                if(!visited[nextx][nexty] && map[nextx][nexty]==1){
+//                    dfs(nextx, nexty);
+//                }
+//            }
+//        }
+//
+//    }
+//}
+
