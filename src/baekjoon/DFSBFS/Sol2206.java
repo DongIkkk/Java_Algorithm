79861bd625b8abefdc2581142dd5ba95aa642fb2
DongIkkk <w8h0412@gmail.com>
2023-04-05 17:42:48 +0900

    0405 BOJ 2206 벽부수고이동하기! 주석 bfs

--- src/baekjoon/DFSBFS/Sol2206.java
+++ src/baekjoon/DFSBFS/Sol2206.java
@@ -47,10 +47,22 @@ public class Sol2206 {
 
                 if(nextx<0 || nexty<0 || nextx>=n || nexty>=m) continue;
 
+                // canbreak == 1 : 하나를 부술 수 있는 상태로 돌아다님
+                // canbreak == 0 : 하나 부수고 돌아다니는 상태
+
+                // visited[][][0] : 벽을 하나 부순 내가 지나다니는 길
+                // visited[][][1] : 안부수고 지나다니는 길
+
+                // 벽을 만났고 부술수 있다
                 if(map[nextx][nexty] == 1 && canbreak == 1) {
+                    // 벽부수고 돌아다니는 길  = 안부수고 돌아다니던 지금 길 + 1
                     visited[nextx][nexty][0] = visited[nowx][nowy][canbreak] + 1;
+                    // 부순 상태로 돌아다니게 하기
                     q.add(new int[] {nextx, nexty, 0});
-                } else if( map[nextx][nexty] == 0 && visited[nextx][nexty][canbreak]==0) {
+                }
+                // 길을 만났고 지금 내 상태로 돌아다니는 길이 방문하지않았다.
+                else if( map[nextx][nexty] == 0 && visited[nextx][nexty][canbreak]==0) {
+                    // 내 상태로 돌아다니는 길에 거리저장
                     visited[nextx][nexty][canbreak] = visited[nowx][nowy][canbreak] + 1;
                     q.add(new int[] {nextx, nexty, canbreak});
                 }
