01a8618515f15d7d46ad074e95b58b6cb8cbd421
DongIkkk <w8h0412@gmail.com>
2023-04-19 17:08:22 +0900

    0419 SWEA 1949 등산로 조성 - dfs refactoring

--- src/swea/SWTest/Sol1949.java
+++ src/swea/SWTest/Sol1949.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class Sol1949 {
     static int N, K, result;
     static int[][] map;
-    static boolean[][][] visited;
+    static boolean[][] visited;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
 
@@ -35,12 +35,10 @@ public class Sol1949 {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if(map[i][j] == max){
-                        visited = new boolean[N][N][2];
-                        visited[i][j][1] = true;
-                        visited[i][j][0] = true;
+                        visited = new boolean[N][N];
+                        visited[i][j] = true;
                         dfs(i, j, 1, 1);
-                        visited[i][j][1] = false;
-                        visited[i][j][0] = false;
+                        visited[i][j] = false;
                     }
                 }
             }
@@ -49,7 +47,7 @@ public class Sol1949 {
     }
 
     static public void dfs(int x, int y, int hklength, int cancon){
-        if(result <= hklength) {
+        if(result < hklength) {
             result = hklength;
         }
 
@@ -58,17 +56,17 @@ public class Sol1949 {
             int nexty = y + dy[i];
             if(nextx<0 || nexty<0 || nextx>=N || nexty>=N) continue;
 
-            if(!visited[nextx][nexty][1] && !visited[nextx][nexty][0] && map[nextx][nexty] < map[x][y]) {
-                visited[nextx][nexty][cancon] = true;
+            if(!visited[nextx][nexty] && map[nextx][nexty] < map[x][y]) {
+                visited[nextx][nexty] = true;
                 dfs(nextx, nexty, hklength + 1, cancon);
-                visited[nextx][nexty][cancon] = false;
-            } else if(cancon == 1 && !visited[nextx][nexty][0] && !visited[nextx][nexty][1]){
+                visited[nextx][nexty] = false;
+            } else if(cancon == 1 && !visited[nextx][nexty]){
                 for (int j = 1; j <= K ; j++) {
                     map[nextx][nexty] -= j;
                     if(map[nextx][nexty] < map[x][y]){
-                        visited[nextx][nexty][0] = true;
+                        visited[nextx][nexty] = true;
                         dfs(nextx, nexty, hklength+1, 0);
-                        visited[nextx][nexty][0] = false;
+                        visited[nextx][nexty] = false;
                     }
                     map[nextx][nexty] += j;
                 }
