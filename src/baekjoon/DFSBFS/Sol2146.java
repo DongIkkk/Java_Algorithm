bb7674d88f21ab061c7f20d7776a1f8fc4e0812c
DongIkkk <w8h0412@gmail.com>
2023-04-24 17:57:36 +0900

    0424 BOJ 2146 다리 만들기 - BFS

--- src/baekjoon/DFSBFS/Sol2146.java
+++ src/baekjoon/DFSBFS/Sol2146.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Sol2146 {
     static int count = 0;
-    static int N;
+    static int N, min;
     static int[][] map;
     static boolean[][] visited;
     static int[] dx = {0, 1, 0, -1};
@@ -18,7 +18,6 @@ public class Sol2146 {
 
         map = new int[N][N];
         visited = new boolean[N][N];
-        Queue<int[]> q = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -34,15 +33,64 @@ public class Sol2146 {
                 }
             }
         }
+        count=1;
+        min = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
-
             for (int j = 0; j < N; j++) {
-                System.out.printf("%d ", map[i][j]);
+                if(map[i][j] == count){
+                    bfs();
+                    count++;
+                }
             }
-            System.out.println();
         }
 
+        System.out.println(min);
+
+
+    }
+
+    public static void bfs(){
+        visited = new boolean[N][N];
+        int[][] dist = new int[N][N];
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(map[i][j]==count){
+                    q.add(new int[] {i, j});
+                    visited[i][j]=true;
+                }
+            }
+        }
+
+        while(q.size()!=0){
+            int[] now = q.poll();
+            int nowx = now[0];
+            int nowy = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nextx = nowx + dx[i];
+                int nexty = nowy + dy[i];
+
+                if(nextx<0 || nexty<0 || nextx>=N || nexty>=N) continue;
+
+                if(map[nextx][nexty]>0 && map[nextx][nexty]!=count){
+                    if(min > dist[nowx][nowy]){
+                        min = dist[nowx][nowy];
+                    }
+                    return;
+                }
+
+                if(map[nextx][nexty]==0 && !visited[nextx][nexty]){
+                    dist[nextx][nexty] = dist[nowx][nowy] + 1;
+
+                    visited[nextx][nexty] = true;
+                    q.add(new int[] {nextx, nexty});
+                }
+            }
+
+        }
 
     }
 
