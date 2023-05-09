fe8d7b6de046fe22f5c7f8ca82ab86e98ce90cd4
DongIkkk <w8h0412@gmail.com>
2023-05-09 22:38:37 +0900

    0509 BOJ 9372 상근이의 여행

---
+++ src/baekjoon/graph/Sol9372.java
@@ -0,0 +1,20 @@
+package graph;
+
+import java.util.Scanner;
+
+public class Sol9372 {
+    static int N, M;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-->0) {
+            N = sc.nextInt();
+            M = sc.nextInt();
+            for (int i = 0; i < M; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+            }
+            System.out.println(N-1);
+        }
+    }
+}
