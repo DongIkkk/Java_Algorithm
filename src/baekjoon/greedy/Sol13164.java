22efaab83d3d956210ab623941e3806162499910
DongIkkk <w8h0412@gmail.com>
2024-01-27 22:51:41 +0900

    0127 BOJ 13164 행복 유치원 - greedy, sort

---
+++ src/baekjoon/greedy/Sol13164.java
@@ -0,0 +1,32 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Sol13164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int[] temp = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(i>=1) temp[i] = arr[i] - arr[i-1];
+        }
+
+        Arrays.sort(temp);
+        int result = 0;
+        for (int i = 0; i <= N-K; i++) {
+            result += temp[i];
+        }
+        System.out.println(result);
+    }
+}
